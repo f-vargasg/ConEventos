b658b52ba8f0248254021457f562b76ad4f094cc
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-03-04 11:44:52 -0600

    Agregar archivos de proyecto.

---
+++ ConEventos/ProcessBusinessLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConEventos
+{
+    public delegate void Notify();
+    class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started...");
+            // some code here
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ ConEventos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += Bl_ProcessCompleted;
+            bl.StartProcess();
+            Console.ReadKey();
+        }
+
+        private static void Bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!!!");
+        }
+    }
+}
